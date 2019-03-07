83588838d092c2090f70ca21a0c449223f99efdc
Unknown <janecka.pavel@gmail.com>
2019-03-07 12:08:18 +0100

    Better export management

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { GamePadHandler } from './gamepad-handler'
+export { AxisInformation } from './axe-utils'
+export { ButtonInformation } from './button-utils'
+export { GamepadMapping, GamepadId } from './gamepad-utils'
